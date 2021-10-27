c5e3917321a3012f3ef699cd5bbd47f2ef177853
Andres Ulloa <andresguc@gmail.conm>
2021-10-27 14:43:09 -0500

    Code: Create a function for adoptions of pets
    - Create a function "Adopt" to add the pet to the Adopter

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.0;
 
 contract Adoption {
 
-    address[16] public adopter; 
+    address[16] public adopter;
+
+    // Adopting a pet
+    function adopt(uint petId) public returns (uint) {
+        require(petId <= 0 && petId <= 16);
+        
+        adopter[petId] = msg.sender;
+
+        return petId;
+    }
     
 }
